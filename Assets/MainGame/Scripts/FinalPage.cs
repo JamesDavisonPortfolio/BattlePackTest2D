0f64fc96c1ed09bc3fba3a260ddec4a7acce4e56
JamesDavisonPortfolio <77494203+JamesDavisonPortfolio@users.noreply.github.com>
2021-04-29 01:05:49 +0100

    Final Workable Model

---
+++ Assets/MainGame/Scripts/FinalPage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FinalPage : MonoBehaviour
+{
+    public TextMeshProUGUI finalScoreText;
+    public TextMeshProUGUI finalMazeScoreText;
+    public TextMeshProUGUI finalBossScoreText;
+    public TextMeshProUGUI finalAnswersWrongText;
+
+    public DataCarrier[] levelDetails;
+
+    int finalScore;
+    int finalMazeScore;
+    int finalBossScore;
+    int finalAnswersWrong;
+
+    void Start()
+    {
+        GetScores();
+        finalScoreText.SetText("Total Score: " + finalScore.ToString("00000"));
+        finalMazeScoreText.SetText("Total Maze Score: " + finalMazeScore.ToString("00000"));
+        finalBossScoreText.SetText("Final Boss Score: " + finalBossScore.ToString("00000"));
+        finalAnswersWrongText.SetText("Total Incorrect Answers: " + finalAnswersWrong.ToString());
+    }
+
+    void GetScores()
+    {
+        for(int i = 0; i < levelDetails.Length; i++)
+        {
+            finalMazeScore += levelDetails[i].GetLevelScore();
+            finalBossScore += levelDetails[i].GetBossScore();
+            finalAnswersWrong += levelDetails[i].GetWrong();
+        }
+        finalScore = (finalMazeScore + finalBossScore);
+    }
+}
